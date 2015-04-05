e55dfada5c8f6de96545b6b17ac75022bc5b44f8
Alexander Seto <alexanderseto@Alexanders-MacBook-Pro.local>
2015-04-05 10:49:38 -0700

    Initial Commit

---
+++ HomeSafe2/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  HomeSafe2
+//
+//  Created by Alexander Seto on 4/5/15.
+//  Copyright (c) 2015 Alexander Seto. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
